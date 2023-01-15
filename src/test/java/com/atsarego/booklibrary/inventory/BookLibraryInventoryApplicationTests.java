372156767a36d2d78079f96c05952e7cb4036994
Andrey Tsaregorodtsev <andreyc86@mail.ru>
2023-01-15 20:32:01 +0600

    Add test step to config.yml

--- src/test/java/com/atsarego/booklibrary/inventory/BookLibraryInventoryApplicationTests.java
+++ src/test/java/com/atsarego/booklibrary/inventory/BookLibraryInventoryApplicationTests.java
@@ -1,6 +1,5 @@
 package com.atsarego.booklibrary.inventory;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,7 +8,6 @@ class BookLibraryInventoryApplicationTests {
 
 	@Test
 	void contextLoads() {
-		Assertions.fail();
 	}
 
 }
